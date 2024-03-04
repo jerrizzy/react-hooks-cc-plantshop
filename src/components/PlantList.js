989f1b0cb38893886593454015f34cbfebbf51f6
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-03-04 14:17:27 -0500

    lab completed

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, filteredList }) {
+
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {filteredList.map((plant) => <PlantCard key={plant.id} plant={plant} />)}
+    </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+const [plants, setPlants] = useState([])
+const [search, setSearch] = useState("")
+
+const filteredList = plants.filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()))
+
+
+useEffect(()=>{
+  fetch("http://localhost:6001/plants")
+  .then((resp) => resp.json())
+  .then((data) => setPlants(data))
+}, [])
+console.log(plants)
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plants={plants} setPlants={setPlants}/>
+      <Search search={search} setSearch={setSearch} />
+      <PlantList filteredList={filteredList} plants={plants} />
     </main>
   );
 }
